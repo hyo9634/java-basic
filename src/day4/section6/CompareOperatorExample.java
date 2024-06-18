2c891decd317e75e4ee8165d168877b43c0a8a05
thatzcool <mycaptain0771@gmail.com>
2024-06-18 09:17:17 +0900

    operator_end

--- src/day4/section6/CompareOperatorExample.java
+++ src/day4/section6/CompareOperatorExample.java
@@ -5,24 +5,45 @@ public class CompareOperatorExample {
 		int num1 = 10;
 		int num2 = 10;
 
-		/*System.out.println("result1: " + result1);
-		System.out.println("result2: " + result2);
-		System.out.println("result3: " + result3);*/
-			
+		if(num1 == num2){
+			System.out.println("같다");
+		}   //num1 과 num2 의 값을 비교하여 같으면 같다. 다르다. 결과 값을 출력
+		else{
+			System.out.println("다르다");
+		}
 
-		//System.out.println("result4: " + result4);
-			
+		if(num1 != num2){
+			System.out.println("다르다");
+		}   //num1 과 num2 의 값을 비교하여 같으면 같다. 다르다. 결과 값을 출력
+		else{
+			System.out.println("같다");
+		}
 
-		//System.out.println("result5: " + result5);
+        char ch = 'A';
+		int  chInt = 65;
+		if(ch == chInt){
+			System.out.println("같다");
+		}   //num1 과 num2 의 값을 비교하여 같으면 같다. 다르다. 결과 값을 출력
+		else{
+			System.out.println("틀리다");
+		}
+		System.out.println(ch == chInt);
+
+		//예외 check!
+		//0.1f  == 0.1
+		System.out.println(0.1f == 0.1);
+		System.out.println(0.1f ==(float)0.1);
+
+		//문자열을 비교시 ,  equals() !equals()
+		String str1 = "안녕";
+		String str2 = "안 녕";
+
+		boolean result = str1.equals(str2);
+		System.out.println(result);
+
+		boolean result1 = ! str1.equals(str2);
+		System.out.println(result1);
 
 
-		/*System.out.println("result6: " + result6);
-		System.out.println("result7: " + result7);*/
-
-
-		/*boolean result8 = (str1.equals(str2));
-		boolean result9 = (! str1.equals(str2));
-		System.out.println("result8: " + result8);
-		System.out.println("result9: " + result9);*/
 	}
 }
