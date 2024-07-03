213497bbbfc605c14da121c88d025f187269c43c
thatzcool <mycaptain0771@gmail.com>
2024-07-03 16:58:51 +0900

    JavaClass_JDBC_homework_ready

--- src/day16/jdbcEx/JdbcEx.java
+++ src/day16/jdbcEx/JdbcEx.java
@@ -2,6 +2,7 @@ package day16.jdbcEx;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,7 +10,124 @@ import java.sql.Statement;
 public class JdbcEx {
 
   public static void main(String[] args) {
-    String url = "jdbc:mysql://localhost:3306/employees?characterEncoding=UTF-8&serverTimezone=UTC";
+    System.out.println("==회원 정보 입력 후 조회===");
+    //회원의 정보를 사용자 받는 기능  (static 메소드로)
+    //Member 클래스(VO)
+    memberInsert( Member member );
+    memberSearch();
+    System.out.println("==회원 정보수정 후 조회===");
+    memberUpdate();
+    memberSearch();
+    System.out.println("==회원 정보삭제 후 조회===");
+    memberDelete();
+    memberSearch();
+
+  }
+
+  private static void memberDelete() {
+    String url = "jdbc:mysql://localhost:3306/employees";
+    String userName = "root";
+    String password = "1234";
+    String query = "DELETE FROM member where id = ? ";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    int result = 0;
+
+    try {
+      //Class.forName("com.mysql.cj.jdbc.Driver");
+      con = DriverManager.getConnection(url, userName, password);
+      System.out.println(con);
+      pstmt = con.prepareStatement(query);
+
+      pstmt.setInt(2, 1);
+
+      result = pstmt.executeUpdate();
+      if (result == 1) {
+        System.out.println("회원 정보가 완전 잘 수정 되었습니다.");
+      } else if (result == 0) {
+        System.out.println("회원 정보 수정이 실패하였습니다.");
+      }
+
+      pstmt.close();
+      con.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void memberUpdate() {
+    String url = "jdbc:mysql://localhost:3306/employees";
+    String userName = "root";
+    String password = "1234";
+    String query = "UPDATE member SET job = ? where id = ? ";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    int result = 0;
+
+    try {
+      //Class.forName("com.mysql.cj.jdbc.Driver");
+      con = DriverManager.getConnection(url, userName, password);
+      System.out.println(con);
+      pstmt = con.prepareStatement(query);
+
+      pstmt.setString(1, "engineer");
+      pstmt.setInt(2, 1);
+
+      result = pstmt.executeUpdate();
+      if (result == 1) {
+        System.out.println("회원 정보가 완전 잘 수정 되었습니다.");
+      } else if (result == 0) {
+        System.out.println("회원 정보 수정이 실패하였습니다.");
+      }
+
+      pstmt.close();
+      con.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  private static void memberInsert() {
+    String url = "jdbc:mysql://localhost:3306/employees";
+    String userName = "root";
+    String password = "1234";
+    String query = "INSERT INTO member VALUES(?,?,?)";
+
+    Connection con = null;
+    PreparedStatement pstmt = null;
+    int result = 0;
+
+    try {
+      //Class.forName("com.mysql.cj.jdbc.Driver");
+      con = DriverManager.getConnection(url, userName, password);
+      System.out.println(con);
+      pstmt = con.prepareStatement(query);
+      pstmt.setInt(1, 4);
+      pstmt.setString(2, "kss");
+      pstmt.setString(3, "professor");
+
+      result = pstmt.executeUpdate();
+      if (result == 1) {
+        System.out.println("회원 정보가 입력되었습니다.");
+      } else if (result == 0) {
+        System.out.println("회원 정보 입력이 실패하였습니다.");
+      }
+
+      pstmt.close();
+      con.close();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+
+
+  }
+
+
+  public static void memberSearch() {
+    String url = "jdbc:mysql://localhost:3306/employees";
     String userName = "root";
     String password = "1234";
     String query = "SELECT * FROM member";
@@ -39,7 +157,6 @@ public class JdbcEx {
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-
 
   }
 
