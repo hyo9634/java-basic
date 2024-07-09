6849435fcf0aef7b697c868a86fd1506b7bde829
thatzcool <mycaptain0771@gmail.com>
2024-07-09 14:56:41 +0900

    JavaClass_api01_cloneable

---
+++ src/day18/objectEx/Address.java
@@ -0,0 +1,20 @@
+package day18.objectEx;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Address implements Cloneable {
+
+  private int code;
+
+  private String addr;
+
+  @Override
+  protected Object clone() throws CloneNotSupportedException {
+    return super.clone();
+  }
+}
